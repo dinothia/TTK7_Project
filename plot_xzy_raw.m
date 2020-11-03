1ed503b1c70ea94def70cf104460bd46538aef71
dinossht <dinosshan1996@hotmail.com>
2020-11-03 09:23:18 +0100

    plot xzy raw fixed, x-y flipped

--- plot_xzy_raw.m
+++ plot_xzy_raw.m
@@ -1,3 +1,4 @@
+%% Load raw data
 load('Data/vanvikan/imu_measurements_1_2')
 
 start_idx = length(imuData.acceleration(1,:))-250*60;
@@ -8,10 +9,20 @@ signal_x = imuData.acceleration(1,start_idx:end_idx);
 signal_y = imuData.acceleration(2,start_idx:end_idx);
 signal_z = imuData.acceleration(3,start_idx:end_idx)-9.81;
 
-collisions = -3+5*imuData.colition_label(start_idx:end_idx);
+%% Transform IMU to body frame
+signal_x = imuData.acceleration(2,start_idx:end_idx);
+signal_y = imuData.acceleration(1,start_idx:end_idx);
+signal_z = -(imuData.acceleration(3,start_idx:end_idx)-9.81);
 
+%% Load collision data
+collisions = 0*imuData.colition_label;
+collisions(950.5*250) = 1;
+collisions(952.5*250) = 1;
+collisions = -3+5*collisions(start_idx:end_idx);
+
+%% Set time vector
 t = imuData.t_imu(start_idx:end_idx);
-t = t - imuData.t_imu(1);
+t = t - t(1);
 
 
 %% Plot signals
@@ -20,7 +31,7 @@ fig = figure(1);
 
 subplot(311);
 plot(t, signal_x, 'color', 'r'); hold on;
-plot(t,collisions, 'color', 'k')
+plot(t,collisions, '--k')
 grid on;
 xlim([t(1),t(end)]);
 ylim([-2,2]);
@@ -30,7 +41,7 @@ legend("x-axis", "contacts along y-axis", 'Location','northwest');
 
 subplot(312);
 plot(t, signal_y, 'color', 'g'); hold on;
-plot(t,collisions, 'color', 'k')
+plot(t,collisions, '--k')
 grid on;
 ylim([-2,2]);
 xlim([t(1),t(end)]);
@@ -40,7 +51,7 @@ legend("y-axis", "contacts along y-axis", 'Location','northwest');
 
 subplot(313);
 plot(t, signal_z, 'color', 'b'); hold on;
-plot(t,collisions, 'color', 'k')
+plot(t,collisions, '--k')
 grid on;
 ylim([-2,2]);
 xlim([t(1),t(end)]);
